8ec2d74dda3deb32ed31fd78e14c4f47069c716e
Aleksandra Wierzbiak <alewierzbiak@gmail.com>
2018-02-11 19:05:20 +0100

    Changing seasons

--- src/objects/Map.js
+++ src/objects/Map.js
@@ -4,7 +4,6 @@
 */
 import Simulator from 'objects/Simulator';
 import Farm from 'objects/Farm';
-
 import Cage from 'objects/Cage';
 import Incubator from 'objects/Incubator';
 import Pavilion from 'objects/Pavilion';
@@ -16,32 +15,33 @@ import KillingStation from 'objects/KillingStation';
 import SkinningStation from 'objects/SkinningStation';
 
 class Map {
-  constructor(game) {
-    this.game = window.game;
-    this.init();
-  }
+  constructor() {}
 
   init() {
     // game settings
+    this.game = window.game;
     this.game.world.setBounds(0, 0, Simulator.settings.width, Simulator.settings.height);
+
+    // set sprites groups
+    this.groundGroup = this.game.add.group();
+    this.foodStorageGroup = this.game.add.group();
+    this.incubatorGroup = this.game.add.group();
+    this.pavilionGroup = this.game.add.group();
+    this.cageGroup = this.game.add.group();
+    this.slaughterhouseGroup = this.game.add.group();
+    this.carcassStorageGroup = this.game.add.group();
+    this.furStorageGroup = this.game.add.group();
 
     this.renderMap();
   }
 
   renderMap() {
     //tiles groups
-    var groundGroup = this.game.add.group();
-    var foodStorageGroup = this.game.add.group();
-    var incubatorGroup = this.game.add.group();
-    var pavilionGroup = this.game.add.group();
-    var cageGroup = this.game.add.group();
-    var slaughterhouseGroup = this.game.add.group();
-    var carcassStorageGroup = this.game.add.group();
-    var furStorageGroup = this.game.add.group();
     var tile, offset;
 
     // draw background
-    var background = this.game.add.sprite(0, 0, 'background', 0, groundGroup);
+    this.background = this.game.add.sprite(0, 0, 'background', 0, this.groundGroup);
+    this.background.tint = '0xb5dd6f';
 
     // draw map with zig zag method
     for(var i = 0; i < Simulator.map.length ; i++) {
@@ -57,13 +57,13 @@ class Map {
         var index = Simulator.map[i][j];
 
         if(index == 1) {
-          tile = new FoodStorage(this.game, x, y, 'food_storage', 0, foodStorageGroup);
+          tile = new FoodStorage(this.game, x, y, 'food_storage', 0, this.foodStorageGroup);
           Farm.foodStorage = tile;
           tile.anchor.set(0.5);
         }
 
         if(index == 2) {
-          tile = new Incubator(this.game, x, y, 'incubator', 0, incubatorGroup);
+          tile = new Incubator(this.game, x, y, 'incubator', 0, this.incubatorGroup);
           Farm.incubators.push(tile);
           tile.anchor.set(0.5);
         }
@@ -74,16 +74,16 @@ class Map {
           var pavilion = type_info[0];
           var enabled = type_info[1];
 
-          tile = new Cage(this.game, x, y, enabled ? 'cage_double_full' : 'cage_double_empty', 0, cageGroup, enabled, pavilion);
+          tile = new Cage(this.game, x, y, enabled ? 'cage_double_full' : 'cage_double_empty', 0, this.cageGroup, enabled, pavilion);
           Farm.cages.push(tile);
           tile.anchor.set(0.65, 0.4);
         }
 
         if(index == 3) {
-          var pavilion_back = this.game.add.sprite(x, y, 'pavilion_back', 0, pavilionGroup);
+          var pavilion_back = this.game.add.sprite(x, y, 'pavilion_back', 0, this.pavilionGroup);
           pavilion_back.anchor.set(0.25, 0.91);
 
-          var pavilion_front = new Pavilion(this.game, x, y, 'pavilion_front', 0, pavilionGroup);
+          var pavilion_front = new Pavilion(this.game, x, y, 'pavilion_front', 0, this.pavilionGroup);
           Farm.pavilions.push(pavilion_front);
           pavilion_front.anchor.set(0.25, 0.91);
         }
@@ -94,47 +94,64 @@ class Map {
         }
 
         if(index == 50) {
-          var slaughterhouse_back = this.game.add.sprite(x, y, 'slaughterhouse_back', 0, slaughterhouseGroup);
+          var slaughterhouse_back = this.game.add.sprite(x, y, 'slaughterhouse_back', 0, this.slaughterhouseGroup);
           slaughterhouse_back.anchor.set(0, 0);
         }
 
         if(index == 5) {
-          var slaughterhouse_front = new Slaughterhouse(this.game, x, y, 'slaughterhouse_front', 0, slaughterhouseGroup);
+          var slaughterhouse_front = new Slaughterhouse(this.game, x, y, 'slaughterhouse_front', 0, this.slaughterhouseGroup);
           Farm.slaughterhouse = slaughterhouse_front;
           slaughterhouse_front.anchor.set(1);
         }
 
         if(index == 6) {
-          tile = new KillingStation(this.game, x, y, 'killing_robot', 0, slaughterhouseGroup);
+          tile = new KillingStation(this.game, x, y, 'killing_robot', 0, this.slaughterhouseGroup);
           Farm.killingStations.push(tile);
           tile.anchor.set(0.5);
         }
 
         if(index == 7) {
-          tile = new SkinningStation(this.game, x, y, 'skinning_robot', 0, slaughterhouseGroup);
+          tile = new SkinningStation(this.game, x, y, 'skinning_robot', 0, this.slaughterhouseGroup);
           Farm.skinningStations.push(tile);
           tile.anchor.set(0.5);
         }
 
         if(index == 8) {
-          var furStorage = new FurStorage(this.game, x, y, 'storage', 0, furStorageGroup);
+          var furStorage = new FurStorage(this.game, x, y, 'storage', 0, this.furStorageGroup);
           Farm.furStorage = furStorage;
           furStorage.anchor.set(0.5);
         }
 
         if(index == 81) {
-          tile = this.game.add.sprite(x, y, 'furs', 0, furStorageGroup);
+          tile = this.game.add.sprite(x, y, 'furs', 0, this.furStorageGroup);
           tile.anchor.set(0.5);
         }
 
         if(index == 9) {
-          var carcassStorage = new CarcassStorage(this.game, x, y, 'carcass', 0, carcassStorageGroup);
+          var carcassStorage = new CarcassStorage(this.game, x, y, 'carcass', 0, this.carcassStorageGroup);
           Farm.carcassStorage = carcassStorage;
           carcassStorage.anchor.set(0.5);
         }
       }
     }
   }
+
+  switchTextures(season) {
+    switch (season) {
+      case 'wiosna':
+        this.background.tint = '0xb5dd6f';
+        break;
+      case 'lato':
+        this.background.tint = '0x76c544';
+        break;
+      case 'jesień':
+        this.background.tint = '0xd68c5a';
+        break;
+      case 'zima':
+        this.background.tint = '0xffffff';
+        break;
+    }
+  }
 }
 
-export default Map;
+export default new Map();
